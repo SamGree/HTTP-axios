e3e71eb8856f5b2ee0ff0d1916f1abb2daebf969
Sam Green <sam.green7777@yahoo.com>
2024-11-07 17:02:17 +0000

    add three files to the components folder

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import HTTPRequests from "./components/HTTPRequests";
+import HTTPPost from "./components/HTTPPost";
+import HTTPHooks from "./components/HTTPHooks";
+
+
+function App() {
+  return <div className="App">
+      <HTTPRequests />
+      <HTTPPost />
+      <HTTPHooks />
+  </div>;
+}
+
+export default App;
